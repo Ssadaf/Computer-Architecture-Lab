4ec3de072b1ab9653a7cef3c38057e48eb30f40f
Ssadaf <s.sadaf2277@gmail.com>
2020-11-25 16:48:16 +0330

    Implement ID_stage

--- ID_stage.v
+++ ID_stage.v
@@ -1,9 +1,61 @@
 module ID_stage (
   input clk, rst, 
-  input [31:0] PC_in, 
-  output [31:0] PC_out
+  input [31:0] PC_in,
+  input [31:0] instruction, 
+  input [31:0] result_wb, 
+  input write_back,
+  input [3:0] dest_wb,
+  input hazard, 
+  input [3:0] status, // Z N C V	
+
+  output [31:0] PC_out,
+  output reg wb_enable, mem_read, mem_write, B, S, imm,
+  output reg [3:0] exec_cmd,
+  output [31:0] val_Rn, val_Rm,
+  output [3:0] Rd,
+  output [11:0] shift_operand,
+  output [23:0] signed_imm_24,
+  //TODO: hazard
+  output [3:0] src1, src2,
+  output two_src 
 );
- 
-  assign   PC_out = PC_in;
-  
+	wire [3:0] cu_exec_cmd;
+	reg [3:0] src2_mux_out;
+	wire cu_mem_read, cu_mem_write, cu_wb_enable, cu_imm, cu_B, cu_S, cond_matched; 
+
+	assign PC_out = PC_in;
+	
+	control_unit cu(instruction[27:26], instruction[24:21], instruction[20], instruction[25],
+	cu_exec_cmd, cu_mem_read, cu_mem_write, cu_wb_enable, cu_imm, cu_B, cu_S);
+	
+	condition_check cc(status[3], status[2], status[1], status[0], instruction[31:28], cond_matched);
+
+	// MUX
+	always @(*) begin
+		if(hazard || !cond_matched) begin
+			{wb_enable, mem_read, mem_write, exec_cmd, B, S, imm} <= 10'b0;
+		end
+
+		else begin
+			{wb_enable, mem_read, mem_write, exec_cmd, B, S, imm} <= {cu_wb_enable, cu_mem_read, cu_mem_write, cu_exec_cmd, cu_B, cu_S, cu_imm};
+		end
+	end
+
+	// MUX
+	always @(*) begin
+		if(wb_enable) begin //TEST
+			src2_mux_out <= instruction[15:12];
+		end
+
+		else begin
+			src2_mux_out <= instruction[19:16];
+		end
+	end
+
+	register_file rf(clk, rst, instruction[19:16], src2_mux_out, dest_wb, result_wb, write_back, val_Rn, val_Rm);
+
+	assign signed_imm_24 = instruction[23:0];
+	assign Rd = instruction[15:12];
+	assign shift_operand = instruction[11:0];
+
 endmodule
--- control_unit.v
+++ control_unit.v
@@ -1,10 +1,9 @@
 module control_unit(
   input [1:0] mode,
   input [3:0] opcode,
-  input S_in,
-  output reg [3:0] exec_command,
-  output mem_read, mem_write,
-  wb_enable, I, B, reg S_out
+  input S_in, I,
+  output reg [3:0] exec_cmd,
+  output mem_read, mem_write, wb_enable, imm, B, reg S_out
 );
   
   assign B = (mode == 2'b10); //B
@@ -22,19 +21,19 @@ module control_unit(
   
   always@(*)begin
     case (opcode)
-      4'b1101  : exec_command <= 4'b0001; //MOV
-      4'b1111  : exec_command <= 4'b1001; //MVN
-      4'b0100  : exec_command <= 4'b0010; //ADD
-      4'b0101  : exec_command <= 4'b0011; //ADC
-      4'b0010  : exec_command <= 4'b0100; //SUB
-      4'b0110  : exec_command <= 4'b0101; //SBC
-      4'b0000  : exec_command <= 4'b0110; //AND
-      4'b1100  : exec_command <= 4'b0111; //ORR
-      4'b0001  : exec_command <= 4'b1000; //EOR
-      4'b1010  : exec_command <= 4'b0100; //CMP
-      4'b1000  : exec_command <= 4'b0110; //TST
-      4'b0100  : exec_command <= 4'b0010; //LDR,STR
-      default : exec_command <= 4'b1111; //B
+      4'b1101  : exec_cmd <= 4'b0001; //MOV
+      4'b1111  : exec_cmd <= 4'b1001; //MVN
+      4'b0100  : exec_cmd <= 4'b0010; //ADD
+      4'b0101  : exec_cmd <= 4'b0011; //ADC
+      4'b0010  : exec_cmd <= 4'b0100; //SUB
+      4'b0110  : exec_cmd <= 4'b0101; //SBC
+      4'b0000  : exec_cmd <= 4'b0110; //AND
+      4'b1100  : exec_cmd <= 4'b0111; //ORR
+      4'b0001  : exec_cmd <= 4'b1000; //EOR
+      4'b1010  : exec_cmd <= 4'b0100; //CMP
+      4'b1000  : exec_cmd <= 4'b0110; //TST
+      4'b0100  : exec_cmd <= 4'b0010; //LDR,STR
+      default : exec_cmd <= 4'b1111; //B
     endcase
   end
 
