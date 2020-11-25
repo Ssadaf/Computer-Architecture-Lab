3194dc0c520f092612a733e2a8f9ab0efb05b566
Ssadaf <s.sadaf2277@gmail.com>
2020-11-25 15:21:48 +0330

    Add comment for condition check

--- condition_check.v
+++ condition_check.v
@@ -5,21 +5,21 @@ module condition_check (
 );
 	always @* begin
 		case(cond)
-			4'b0000: matched <= Z;
-			4'b0001: matched <= !Z;
-			4'b0010: matched <= C;
-			4'b0011: matched <= !C;
-			4'b0100: matched <= N;
-			4'b0101: matched <= !N;
-			4'b0110: matched <= V;
-			4'b0111: matched <= !V;
-			4'b1000: matched <= C && (!Z);
-			4'b1001: matched <= (!C) && Z;
-			4'b1010: matched <= (N == V);
-			4'b1011: matched <= (N != V);
-			4'b1100: matched <= (!Z) && (N == V);
-			4'b1101: matched <= Z || (N != V);
-			4'b1110: matched <= 1;
+			4'b0000: matched <= Z; //EQ
+			4'b0001: matched <= !Z; // NE
+			4'b0010: matched <= C; // CS
+			4'b0011: matched <= !C; //CC
+			4'b0100: matched <= N; // MI
+			4'b0101: matched <= !N; // PL
+			4'b0110: matched <= V; // VS
+			4'b0111: matched <= !V; //VC
+			4'b1000: matched <= C && (!Z); //HI
+			4'b1001: matched <= (!C) && Z; // LS
+			4'b1010: matched <= (N == V); // GE
+			4'b1011: matched <= (N != V); // LT
+			4'b1100: matched <= (!Z) && (N == V); //GT
+			4'b1101: matched <= Z || (N != V); // LE
+			4'b1110: matched <= 1; //AL
 			default: matched <= 0;
 		endcase
 	end
