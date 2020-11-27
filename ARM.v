a6491a0aefa7089c08b7ea148d25db65d010c7c2
Ssadaf <s.sadaf2277@gmail.com>
2020-11-27 18:57:09 +0330

    Modify ID_stage_reg

--- ARM.v
+++ ARM.v
@@ -5,30 +5,41 @@ module ARM (
 reg hazard = 1'b0;
 reg branch_taken = 1'b0;
 reg [31:0] branch_addr = 32'b0;
-reg [3:0] status;
+reg [3:0] status = 4'b0;
 
 wire [31:0] PC_IF_out, PC_ID_in, PC_ID_out, PC_exec_in, PC_exec_out, PC_mem_in, PC_mem_out, PC_WB_in, PC_WB_out;
 wire [31:0] inst_IF, inst_ID;
 
 // WB outputs
-wire [31:0] wb_result_WB;
-wire [3:0] wb_dest_WB;
+reg [31:0] wb_result_WB = 0;
+reg [3:0] wb_dest_WB = 0;
 reg wb_enable_WB = 0;
-wire wb_enable_ID_out;
 
-wire mem_read_ID_out, mem_write_ID_out, B_ID_out, S_ID_out, imm_ID_out;
+wire wb_enable_ID_out, mem_read_ID_out, mem_write_ID_out, B_ID_out, S_ID_out, imm_ID_out;
 wire [3:0] exec_cmd_ID_out, Rd_ID_out;
 wire [31:0] val_Rn_ID_out, val_Rm_ID_out;
-wire [11:0] shift_operand_ID;
-wire [23:0] signed_imm_24_ID;
+wire [11:0] shift_operand_ID_out;
+wire [23:0] signed_imm_24_ID_out;
+
+wire wb_enable_exec_in, mem_read_exec_in, mem_write_exec_in, B_exec_in, S_exec_in, imm_exec_in;
+wire [3:0] exec_cmd_exec_in, Rd_exec_in;
+wire [31:0] val_Rn_exec_in, val_Rm_exec_in;
+wire [11:0] shift_operand_exec_in;
+wire [23:0] signed_imm_24_exec_in;
 
 IF_stage IF(clk, rst, hazard, branch_taken, branch_addr, PC_IF_out, inst_IF);
 IF_stage_reg IF_reg(clk, rst, hazard, branch_taken, PC_IF_out, inst_IF, PC_ID_in, inst_ID);
 
 ID_stage ID(clk, rst, PC_ID_in, inst_ID, wb_result_WB, wb_enable_WB, wb_dest_WB, hazard, status,
 PC_ID_out, wb_enable_ID_out, mem_read_ID_out, mem_write_ID_out, B_ID_out, S_ID_out, imm_ID_out,
-exec_cmd_ID_out, val_Rn_ID_out, val_Rm_ID_out, Rd_ID_out, shift_operand_ID, signed_imm_24_ID);
-ID_stage_reg ID_reg(clk, rst, branch_taken, PC_ID_out, PC_exec_in);
+exec_cmd_ID_out, val_Rn_ID_out, val_Rm_ID_out, Rd_ID_out, shift_operand_ID_out, signed_imm_24_ID_out);
+
+ID_stage_reg ID_reg(clk, rst, branch_taken, PC_ID_out, wb_enable_ID_out, mem_read_ID_out,
+mem_write_ID_out, B_ID_out, S_ID_out, imm_ID_out, exec_cmd_ID_out, val_Rn_ID_out,val_Rm_ID_out,
+Rd_ID_out, shift_operand_ID_out, signed_imm_24_ID_out,
+PC_exec_in, wb_enable_exec_in, mem_read_exec_in, mem_write_exec_in, B_exec_in, S_exec_in,
+imm_exec_in, exec_cmd_exec_in, Rd_exec_in, val_Rn_exec_in, val_Rm_exec_in,
+shift_operand_exec_in, signed_imm_24_exec_in);
 
 exec_stage exec(clk, rst, PC_exec_in, PC_exec_out);
 exec_stage_reg exec_reg(clk, rst, PC_exec_out, PC_mem_in);
--- ID_stage_register.v
+++ ID_stage_register.v
@@ -1,15 +1,51 @@
 module ID_stage_reg(
   input clk, rst, flush, 
   input [31:0] PC_in,
-  output reg [31:0] PC_out
+  input wb_enable_in, mem_read_in, mem_write_in, B_in, S_in, imm_in,
+  input [3:0] exec_cmd_in,
+  input [31:0] val_Rn_in, val_Rm_in,
+  input [3:0] Rd_in,
+  input [11:0] shift_operand_in,
+  input [23:0] signed_imm_24_in,
+  
+  output reg [31:0] PC_out,
+  output reg wb_enable_out, mem_read_out, mem_write_out, B_out, S_out, imm_out,
+  output reg [3:0] exec_cmd_out,
+  output reg [31:0] val_Rn_out, val_Rm_out,
+  output reg [3:0] Rd_out,
+  output reg [11:0] shift_operand_out,
+  output reg [23:0] signed_imm_24_out
 );
   always@(posedge clk, posedge rst) begin
-    if(rst)
+    if(rst)begin
       PC_out <= 32'b0;
-    else if(flush)
+      {wb_enable_out, mem_read_out, mem_write_out, B_out, S_out, imm_out} <= 6'b0;
+      {val_Rn_out, val_Rm_out, Rd_out} <= 68'b0;
+      {exec_cmd_out, shift_operand_out, signed_imm_24_out} <= 40'b0;
+    end
+    
+    else if(flush) begin
       PC_out <= 32'b0;
-    else
+      {wb_enable_out, mem_read_out, mem_write_out, B_out, S_out, imm_out} <= 6'b0;
+      {val_Rn_out, val_Rm_out, Rd_out} <= 68'b0;
+      {exec_cmd_out, shift_operand_out, signed_imm_24_out} <= 40'b0;
+    end
+    
+    else begin
       PC_out <= PC_in;
+      wb_enable_out <= wb_enable_in;
+      mem_read_out <= mem_read_in;
+      mem_write_out <= mem_write_in;
+      B_out <= B_in;
+      S_out <= S_in;
+      imm_out <= imm_in;
+      exec_cmd_out <= exec_cmd_in;
+      val_Rn_out <= val_Rn_in;
+      val_Rm_out <= val_Rm_in;
+      Rd_out <= Rd_in;
+      shift_operand_out <= shift_operand_in;
+      signed_imm_24_out <= signed_imm_24_in;
+    end
   end
 endmodule  
 
