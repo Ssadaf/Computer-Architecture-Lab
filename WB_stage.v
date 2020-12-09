1e07bf566fbcc4c648fc6639ce7216ba4fd04c79
Ssadaf <s.sadaf2277@gmail.com>
2020-12-09 12:46:57 +0330

    Implement Writeback stage

--- WB_stage.v
+++ WB_stage.v
@@ -1,8 +1,20 @@
 module WB_stage (
   input clk, rst, 
   input [31:0] PC_in, 
-  output [31:0] PC_out
+  input mem_read,
+  input [31:0] mem_res, alu_res,
+
+  output [31:0] PC_out,
+  output reg [31:0] wb_val
 );
+
   assign PC_out = PC_in;
+
+  always @(*)begin
+	  if(mem_read)
+	  	wb_val <= mem_res;
+	  else
+	  	wb_val <= alu_res;
+  end
 
 endmodule
