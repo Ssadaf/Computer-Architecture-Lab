8a93429b204d19f5917bd7bf22ce01ea0595ee50
Mahta.r <m77rafiee@yahoo.com>
2020-12-09 11:24:29 +0330

    Add memory stage and hazard detection

--- mem_stage.v
+++ mem_stage.v
@@ -1,9 +1,11 @@
 module mem_stage (
-  input clk, rst, 
-  input [31:0] PC_in, 
-  output [31:0] PC_out
+  input clk, rst,
+  input [31:0] PC_in, ALU_result, val_Rm,
+  input mem_read, mem_write,
+  output [31:0] PC_out, mem_result
 );
 
-  assign   PC_out = PC_in;
+  assign PC_out = PC_in;
+  memory mem(clk, mem_read, mem_write, ALU_result, val_Rm, mem_result);
   
 endmodule
