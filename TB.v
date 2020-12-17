0b6f7de18c19f55df9ec1ca8b3db4700a9f32d18
Mahta.r <m77rafiee@yahoo.com>
2020-12-17 22:55:38 +0330

    Turn forwarding on in testbench

--- TB.v
+++ TB.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ns
 module TB();
-  reg clk = 1'b0, rst = 1'b1, forwarding = 1'b0;
+  reg clk = 1'b0, rst = 1'b1, forwarding = 1'b1;
   ARM processor(clk, rst, forwarding);
   initial begin
     #10 rst = ~rst;
